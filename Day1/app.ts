2ba9c099e5f02d298e0702384d8ffd42d1c29170
Kapitonov Roman Aleksandrovich <r.kapitonov@tcsbank.ru>
2019-01-27 19:13:56 +0300

    HomeWork2 (+задание с урока)

--- Day1/app.ts
+++ Day1/app.ts
@@ -2,7 +2,21 @@ import { read } from "./console";
 
 async function main(): Promise<void>
 {
-    //Здесь необходимо писать реализацию
+    console.log('введите один символ:');
+    var x = await read();
+
+    if ((x>='a') && (x<='z')) 
+    {
+        console.log(x, '= латиница')
+    }
+        else if ((x>='A') && (x<='Z'))
+    {
+        console.log(x, '= латиница')
+    }
+        else 
+    {
+        console.log(x, '= ne latinica')
+    }
 }
 
 main();
--- Day2/app.ts
+++ Day2/app.ts
@@ -2,7 +2,39 @@ import { read } from "./console";
 
 async function main(): Promise<void>
 {
-    //Здесь необходимо писать реализацию
+    /*В программу вводят произвольное число, а программа должна вывести в от словосочетание со словом рубль, в правильном склонении
+К примеру:
+
+- 1 рубль
+- 3 рубля
+- и т. д.*/
+
+    console.log('введите число:');
+    var x = Number(await read());
+    enum RUB {rubley = 'рублей', rubl = 'рубль', rublya = 'рубля'};
+
+    if ((x>20) || (x<10))
+{
+    var xx = x%10;
+    
+    if (xx===0) {
+        console.log(x, RUB.rubley);
+    } else if (xx===1) {
+        console.log(x, RUB.rubl);
+    } else if ((xx>1) && (xx<5)) {
+        console.log(x, RUB.rublya);
+    } else if (xx>4) {
+        console.log(x, RUB.rubley)
+    } 
+
+
+}  else {
+         console.log(x, RUB.rubley)
+        }
+
+    
+
+
 }
 
 main();
