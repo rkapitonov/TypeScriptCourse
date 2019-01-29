fb26bd69bb9182ca56ceb0dadd4b0a622d0e4fd7
Kapitonov Roman Aleksandrovich <r.kapitonov@tcsbank.ru>
2019-01-29 16:53:31 +0300

    WorkTask3

--- Day3/app.ts
+++ Day3/app.ts
@@ -2,7 +2,20 @@ import { read } from "./console";
 
 async function main(): Promise<void>
 {
-    //Здесь необходимо писать реализацию
+    console.log('Введите строку');
+    var x = await read();
+    var afterSplit = x.split(' ');
+    var b = afterSplit.length;
+    var arr: string[] = [];
+    var answer: string = '';
+    var j = -1;
+    for (var i=afterSplit.length; i>0; i--) {
+        
+        arr[j+1] = afterSplit[i-1];
+        answer += arr[j+1] + ' ';
+        
+    }
+    console.log(answer);
 }
 
 main();
