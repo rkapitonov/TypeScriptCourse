ed05a19df4b3d822f61da6671b133ae63e8b1f23
Kapitonov Roman Aleksandrovich <r.kapitonov@tcsbank.ru>
2019-02-01 12:01:46 +0300

    домашнее задание 3

--- Day3/app3.ts
+++ Day3/app3.ts
@@ -1,31 +1,5 @@
 import { read } from "./console";
-
-function generateMassive(sizeMassive:number, countMassive: number): number[] //генерация arrays
-{
-    var massive: number[] = [];
-    var mnogoMassivov: Array<number[]>;
-    
-    for (var i; i<sizeMassive; i++) {
-        massive[i] = (Math.trunc((Math.random()*200) - 50));
-    }
-    
-    for (let j=0; j<countMassive; i++) {
-
-        mnogoMassivov[]
-
-    }
-    
-    
-    
-    
-    
-    console.log(massive);
-    return massive;
-    
-}
-
-
-
+import { join } from "path";
 
 async function main(): Promise<void>
 {
@@ -38,17 +12,59 @@ async function main(): Promise<void>
 Генерация массивов и подсчет суммы их элементов оформить в виде отдельных функций.
 */
 
-
 console.log('Введите размер массивов:');
 var sizeMassive = Number(await read());
 console.log('Введите количество массивов:');
-var countMassive = Number(await read())
-
+var countMassive = Number(await read());
 
 generateMassive(sizeMassive, countMassive);
-
-
 
 }
 
 main();
+
+
+
+function generateMassive(sizeMassive:number, countMassive: number): number[] //Array<number[]> //генерация arrays
+{
+    var maxSum = 0;
+    var nomerMassiva = 0;
+    var massive = new Array(sizeMassive);
+   
+    for (var i = 0; i<countMassive; i++) 
+    {
+        massive[i] = new Array(countMassive);
+        
+        for (var j=0; j<sizeMassive; j++) 
+        {
+            massive[i][j] = (Math.trunc((Math.random()*200) - 50));
+            
+        }
+        var sum = arraySum(massive[i]);
+        console.log('Сумма ', i+1, 'массива = ', sum);
+
+    //максимальная сумма + nomer massiva        
+        
+            if (maxSum < sum)
+                    {
+                        maxSum = sum;
+                        nomerMassiva = i;
+                    }
+ 
+    }
+    console.log(massive);
+    console.log('Max Sum = ', maxSum, '. Номер массива = ', nomerMassiva+1, ' ', massive[nomerMassiva]);     
+    return massive;
+
+}
+
+
+function arraySum(array): number {
+
+    var sum = 0;
+    for(var i = 0; i < array.length; i++){
+        sum += array[i];
+        }
+   // console.log(sum);
+        return sum;
+}
