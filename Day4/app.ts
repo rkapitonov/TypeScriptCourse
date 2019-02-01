ad8942e549156545d1f877d87792cfec1921fa7c
Kapitonov Roman Aleksandrovich <r.kapitonov@tcsbank.ru>
2019-02-01 18:04:36 +0300

    Day4

--- Day4/app.ts
+++ Day4/app.ts
@@ -1,8 +1,41 @@
 import { read } from "./console";
 
+/*
+Создайте транспортное средство, которое состоит из двигателя, колес и бензобака. 
+Все внутренние части тс должны взаимодействовать друг с другом. 
+Внутренний механизм должен быть скрыт от клиента. 
+Все, что ему нужно знать это accelerate(), brake(), turn() - 
+эти методы должны быть частью интерфейса Drivable. 
+Создайте автомобиль.
+*/
+
 async function main(): Promise<void>
 {
-    //Здесь необходимо писать реализацию
+    
 }
 
 main();
+
+
+// carWheel - koleso
+// carEngine - dvigatel
+// carBenzobak - benzobak
+
+interface IDrivable {
+    accelerate(): number;
+    brake(): number;
+    turn(): number;
+}
+
+class Car {
+    private carWheel: number;
+    private carEngine: string;
+    private carBenzobak: number;
+
+
+
+
+
+
+
+}
